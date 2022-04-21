b80939db642629de1f6013771dce636ddf109403
WilliamMCodes <william.moulding8905@OTech.edu>
2022-04-20 18:04:16 -0600

    Added Shooping Cart and Shopping Cart Tests

---
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    public class ShoppingCart
+    {
+        private Customer _customer;
+        private ShoppingCartItem _product1;
+        private ShoppingCartItem _product2;
+        private ShoppingCartItem _product3;
+
+        public ShoppingCart(Customer cust)
+        {
+            _customer = cust;
+        }
+
+        public int GetCustomerId()
+        {
+            return _customer.GetId();
+        }
+
+        public void AddProduct(Product prod, int quantity)
+        {
+            for(int i = 1; i <= 3; i++)
+            {
+                if(GetProduct(i) != null)
+                {
+                    if(GetProduct(i).GetProduct().GetId() == prod.GetId())
+                    {
+                        GetProduct(i).SetQuantity(GetProduct(i).GetQuantity() + quantity);
+                        return;
+                    }
+                }
+                else if(GetProduct(i) == null)
+                {
+                    switch (i)
+                    {
+                        case 1:
+                            _product1 = new ShoppingCartItem(prod, quantity);
+                            return;
+                        case 2:
+                            _product2 = new ShoppingCartItem(prod, quantity);
+                            return;
+                        case 3:
+                            _product3 = new ShoppingCartItem(prod, quantity);
+                            return;
+                        default:
+                            return;
+                    }
+                }
+            }
+        }
+
+        public void AddProduct(Product prod)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                if (GetProduct(i) != null)
+                {
+                    if (GetProduct(i).GetProduct().GetId() == prod.GetId())
+                    {
+                        GetProduct(i).SetQuantity(GetProduct(i).GetQuantity() + 1);
+                        return;
+                    }
+                }
+                else if (GetProduct(i) == null)
+                {
+                    switch (i)
+                    {
+                        case 1:
+                            _product1 = new ShoppingCartItem(prod, 1);
+                            return;
+                        case 2:
+                            _product2 = new ShoppingCartItem(prod, 1);
+                            return;
+                        case 3:
+                            _product3 = new ShoppingCartItem(prod, 1);
+                            return;
+                        default:
+                            return;
+                    }
+                }
+            }
+        }
+
+        public void RemoveProduct(Product prod, int quantity)
+        {
+            if(GetProductById(prod.GetId()) != null)
+            {
+                if (GetProductById(prod.GetId()).GetQuantity() > quantity)
+                {
+                    GetProductById(prod.GetId()).SetQuantity(GetProductById(prod.GetId()).GetQuantity() - quantity);
+                    return;
+                }
+                else
+                {
+                    if(GetProductById(prod.GetId()) == _product1)
+                    {
+                        _product1 = null;
+                        return;
+                    }
+                    else if(GetProductById(prod.GetId()) == _product2)
+                    {
+                        _product2 = null;
+                        return;
+                    }
+                    else
+                    {
+                        _product3 = null;
+                        return;
+                    }
+                }
+            }
+        }
+
+        public void EmptyCart()
+        {
+            _product1 = null;
+            _product2 = null;
+            _product3 = null;
+        }
+
+        public ShoppingCartItem GetProductById(int id)
+        {
+            for(int i = 1; i <= 3; i++)
+            {
+                if (GetProduct(i) != null)
+                {
+                    if (GetProduct(i).GetProduct().GetId() == id)
+                    {
+                        return GetProduct(i);
+                    }
+                }
+            }
+            return null;
+        }
+
+        public decimal GetTotal()
+        {
+            decimal total = (decimal)0.0;
+            for(int i = 1; i <= 3; i++)
+            {
+                if(GetProduct(i) != null)
+                {
+                    total += GetProduct(i).GetProduct().GetPrice() * GetProduct(i).GetQuantity();
+                }
+            }
+            return total;
+        }
+
+        public ShoppingCartItem GetProduct(int productNum)
+        {
+            switch(productNum)
+            {
+                case 1:
+                    return _product1;
+                case 2:
+                    return _product2;
+                case 3:
+                    return _product3;
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ TestCKKProject/ShoppingCartTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestCKKProject
+{
+    public class ShoppingCartTests
+    {
+        CKK.Logic.Models.Customer customerAlpha = new CKK.Logic.Models.Customer();
+        CKK.Logic.Models.Customer customerBravo = new CKK.Logic.Models.Customer();
+        CKK.Logic.Models.Customer customerCharlie = new CKK.Logic.Models.Customer();
+
+        CKK.Logic.Models.ShoppingCart shoppingCartFull;
+        CKK.Logic.Models.ShoppingCart shoppingCartEmpty;
+        CKK.Logic.Models.ShoppingCart shoppingCartTwoItems;
+
+        CKK.Logic.Models.Product productA = new CKK.Logic.Models.Product(); //123456 $2.43 
+        CKK.Logic.Models.Product productB = new CKK.Logic.Models.Product(); //548769 $1.16
+        CKK.Logic.Models.Product productC = new CKK.Logic.Models.Product(); //813442 $0.36
+        CKK.Logic.Models.Product productD = new CKK.Logic.Models.Product(); //664211 $111.25
+        CKK.Logic.Models.Product productE = new CKK.Logic.Models.Product(); //552143 $24.98
+
+        //preps the fields, then tests the carts to make sure items were correctly added
+        [Fact]
+        public void PrepTests()
+        {
+            customerAlpha.SetId(111111);
+            customerBravo.SetId(222222);
+            customerCharlie.SetId(333333);
+            shoppingCartFull = new CKK.Logic.Models.ShoppingCart(customerAlpha);
+            shoppingCartEmpty = new CKK.Logic.Models.ShoppingCart(customerBravo);
+            shoppingCartTwoItems = new CKK.Logic.Models.ShoppingCart(customerCharlie);
+            productA.SetId(123456);
+            productA.SetPrice((decimal)2.43);
+            productB.SetId(548769);
+            productB.SetPrice((decimal)1.16);
+            productC.SetId(813442);
+            productC.SetPrice((decimal)0.36);
+            productD.SetId(664211);
+            productD.SetPrice((decimal)111.25);
+            productE.SetId(552143);
+            productE.SetPrice((decimal)24.98);
+
+            shoppingCartFull.AddProduct(productA, 2);
+            shoppingCartFull.AddProduct(productC);
+            shoppingCartFull.AddProduct(productD);
+            shoppingCartFull.AddProduct(productB);
+
+            shoppingCartTwoItems.AddProduct(productE);
+            shoppingCartTwoItems.AddProduct(productB);
+            shoppingCartTwoItems.AddProduct(productE);
+            shoppingCartTwoItems.AddProduct(productE);
+
+            //added in preparation for total test
+            shoppingCartEmpty.AddProduct(productA, 2);
+            shoppingCartEmpty.AddProduct(productB, 8);
+        }
+        //makes sure that the proper quantities were entered and that product b wasn't added because the cart was full
+        [Fact]
+        public void ShoppingCartFullTest()
+        {
+
+            PrepTests();
+            Assert.True(shoppingCartFull.GetProduct(1).GetQuantity() + shoppingCartFull.GetProduct(2).GetQuantity() +
+                shoppingCartFull.GetProduct(3).GetQuantity() == 4 && shoppingCartFull.GetProductById(productB.GetId()) == null);
+        }
+
+        //makes sure that items are added to like items and not just in empty spaces
+        [Fact]
+        public void ShoppingcartTwoItemsTest()
+        {
+            PrepTests();
+            Assert.True(shoppingCartTwoItems.GetProduct(1).GetProduct().GetId() == productE.GetId() && shoppingCartTwoItems.GetProduct(2).GetProduct().GetId() == productB.GetId()
+                && shoppingCartTwoItems.GetProduct(3) == null && shoppingCartTwoItems.GetProduct(1).GetQuantity() == 3);
+        }
+
+
+        [Fact]
+        public void TestRemoveProductShoppingCartFull()
+        {
+            PrepTests();
+            shoppingCartFull.RemoveProduct(productA, 1);
+            shoppingCartFull.RemoveProduct(productC, 1);
+            shoppingCartFull.RemoveProduct(productD, 1);
+
+            Assert.True(shoppingCartFull.GetProduct(1).GetQuantity() == 1 && shoppingCartFull.GetProduct(2) == null);
+        }
+
+        [Fact]
+        public void TestRemoveProductShoppingCartTwoItems()
+        {
+            PrepTests();
+            shoppingCartTwoItems.RemoveProduct(productE, 1);
+            shoppingCartTwoItems.RemoveProduct(productB, 2);
+
+            Assert.True(shoppingCartTwoItems.GetProductById(productB.GetId()) == null && shoppingCartTwoItems.GetProductById(productE.GetId()).GetQuantity() == 2);
+        }
+
+        [Fact]
+        public void TestGetTotal()
+        {
+            PrepTests();
+            Assert.True(shoppingCartFull.GetTotal() == (productA.GetPrice() * 2 + productC.GetPrice() + productD.GetPrice()) &&
+                shoppingCartTwoItems.GetTotal() == productE.GetPrice() * 3 + productB.GetPrice() &&
+                shoppingCartEmpty.GetTotal() == productA.GetPrice() * 2 + productB.GetPrice() * 8);
+        }
+
+        [Fact]
+        public void TestEmptyCart()
+        {
+            PrepTests();
+            shoppingCartEmpty.EmptyCart();
+
+            Assert.True(shoppingCartEmpty.GetProduct(1) == null);
+        }
+    }
+}
