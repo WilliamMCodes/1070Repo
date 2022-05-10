b70bb000196fd3bcc5182ca1a1febd44f9583ea5
WilliamMCodes <william.moulding8905@OTech.edu>
2022-05-10 17:49:36 -0600

    added IShoppingCart and IStore

---
+++ CKK.Logic/Interfaces/IStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CKK.Logic.Models;
+
+namespace CKK.Logic.Interfaces
+{
+    public interface IStore
+    {
+        StoreItem AddStoreItem(Product prod, int quantity);
+
+        StoreItem RemoveStoreItem(int id, int quantity);
+
+        StoreItem FindItemById(int id);
+
+        List<StoreItem> GetStoreItems();
+    }
+}
