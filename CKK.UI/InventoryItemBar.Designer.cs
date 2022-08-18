ece36bce260b8393df726007a9b9825be1fa358e
WilliamMCodes <william.erin.moulding@gmail.com>
2022-08-17 19:32:55 -0600

    almost there

---
+++ CKK.UI/InventoryItemBar.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace CKK.GUI.WinForms
+{
+    partial class InventoryItemBar
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxItemSelect = new System.Windows.Forms.CheckBox();
+            this.itemIDLabel = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.notesTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBoxItemSelect
+            // 
+            this.checkBoxItemSelect.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBoxItemSelect.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBoxItemSelect.Location = new System.Drawing.Point(3, 3);
+            this.checkBoxItemSelect.Name = "checkBoxItemSelect";
+            this.checkBoxItemSelect.Size = new System.Drawing.Size(25, 25);
+            this.checkBoxItemSelect.TabIndex = 0;
+            this.checkBoxItemSelect.UseVisualStyleBackColor = true;
+            // 
+            // itemIDLabel
+            // 
+            this.itemIDLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.itemIDLabel.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.itemIDLabel.Location = new System.Drawing.Point(30, 0);
+            this.itemIDLabel.Name = "itemIDLabel";
+            this.itemIDLabel.Size = new System.Drawing.Size(118, 30);
+            this.itemIDLabel.TabIndex = 1;
+            this.itemIDLabel.Text = "#-#####-#####-#";
+            this.itemIDLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.descriptionTextBox.Location = new System.Drawing.Point(148, 0);
+            this.descriptionTextBox.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.descriptionTextBox.MaximumSize = new System.Drawing.Size(0, 30);
+            this.descriptionTextBox.MinimumSize = new System.Drawing.Size(0, 30);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.ReadOnly = true;
+            this.descriptionTextBox.Size = new System.Drawing.Size(395, 30);
+            this.descriptionTextBox.TabIndex = 2;
+            this.descriptionTextBox.Text = "Name/Description of Item";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(548, 6);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.MaximumSize = new System.Drawing.Size(120, 0);
+            this.numericUpDown1.MinimumSize = new System.Drawing.Size(90, 0);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.priceTextBox.Location = new System.Drawing.Point(672, 0);
+            this.priceTextBox.Margin = new System.Windows.Forms.Padding(0);
+            this.priceTextBox.MaximumSize = new System.Drawing.Size(0, 30);
+            this.priceTextBox.MinimumSize = new System.Drawing.Size(0, 30);
+            this.priceTextBox.Multiline = true;
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.ReadOnly = true;
+            this.priceTextBox.Size = new System.Drawing.Size(130, 30);
+            this.priceTextBox.TabIndex = 4;
+            this.priceTextBox.Text = "USD:";
+            this.priceTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // notesTextBox
+            // 
+            this.notesTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.notesTextBox.Location = new System.Drawing.Point(802, 0);
+            this.notesTextBox.Margin = new System.Windows.Forms.Padding(0);
+            this.notesTextBox.MaximumSize = new System.Drawing.Size(0, 30);
+            this.notesTextBox.MinimumSize = new System.Drawing.Size(0, 30);
+            this.notesTextBox.Multiline = true;
+            this.notesTextBox.Name = "notesTextBox";
+            this.notesTextBox.ReadOnly = true;
+            this.notesTextBox.Size = new System.Drawing.Size(458, 30);
+            this.notesTextBox.TabIndex = 5;
+            this.notesTextBox.Text = "Notes:";
+            // 
+            // InventoryItemBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.notesTextBox);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.itemIDLabel);
+            this.Controls.Add(this.checkBoxItemSelect);
+            this.Name = "InventoryItemBar";
+            this.Size = new System.Drawing.Size(1260, 30);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxItemSelect;
+        private System.Windows.Forms.Label itemIDLabel;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.TextBox notesTextBox;
+    }
+}
---
+++ CKK.UI/InventoryMultiTab.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace CKK.GUI.WinForms
+{
+    partial class InventoryMultiTab
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.inventoryTab = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.userIDLabel = new System.Windows.Forms.Label();
+            this.accessLevelLabel = new System.Windows.Forms.Label();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.inventoryTab);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.ItemSize = new System.Drawing.Size(150, 25);
+            this.tabControl1.Location = new System.Drawing.Point(0, 150);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1316, 570);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // inventoryTab
+            // 
+            this.inventoryTab.BackColor = System.Drawing.Color.White;
+            this.inventoryTab.Location = new System.Drawing.Point(4, 29);
+            this.inventoryTab.Name = "inventoryTab";
+            this.inventoryTab.Padding = new System.Windows.Forms.Padding(3);
+            this.inventoryTab.Size = new System.Drawing.Size(1308, 537);
+            this.inventoryTab.TabIndex = 0;
+            this.inventoryTab.Text = "Store Inventory";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1308, 537);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "other tabs as needed";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // userIDLabel
+            // 
+            this.userIDLabel.Location = new System.Drawing.Point(4, 4);
+            this.userIDLabel.Name = "userIDLabel";
+            this.userIDLabel.Size = new System.Drawing.Size(274, 24);
+            this.userIDLabel.TabIndex = 1;
+            this.userIDLabel.Text = "User ID Label";
+            this.userIDLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // accessLevelLabel
+            // 
+            this.accessLevelLabel.Location = new System.Drawing.Point(284, 4);
+            this.accessLevelLabel.Name = "accessLevelLabel";
+            this.accessLevelLabel.Size = new System.Drawing.Size(274, 24);
+            this.accessLevelLabel.TabIndex = 2;
+            this.accessLevelLabel.Text = "Permission Level:";
+            this.accessLevelLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.BackColor = System.Drawing.Color.Red;
+            this.logoutButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.logoutButton.Location = new System.Drawing.Point(1237, 5);
+            this.logoutButton.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(75, 25);
+            this.logoutButton.TabIndex = 3;
+            this.logoutButton.Text = "Log Out";
+            this.logoutButton.UseVisualStyleBackColor = false;
+            // 
+            // InventoryMultiTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.accessLevelLabel);
+            this.Controls.Add(this.userIDLabel);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "InventoryMultiTab";
+            this.Size = new System.Drawing.Size(1316, 723);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage inventoryTab;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label userIDLabel;
+        private System.Windows.Forms.Label accessLevelLabel;
+        private System.Windows.Forms.Button logoutButton;
+    }
+}
