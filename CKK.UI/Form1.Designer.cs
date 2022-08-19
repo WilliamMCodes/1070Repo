dd8a726b7f00a16321652a07f3ad55877055d979
William Moulding <william.moulding8905@omail.otech.edu>
2022-08-18 20:02:09 -0600

    done f**ed up

--- CKK.UI/Form1.Designer.cs
+++ CKK.UI/Form1.Designer.cs
@@ -29,19 +29,83 @@ namespace CKK.GUI.WinForms
         /// </summary>
         private void InitializeComponent()
         {
+            this.loginTool1 = new CKK.GUI.WinForms.LoginTool();
+            this.inventoryMultiTab1 = new CKK.GUI.WinForms.InventoryMultiTab();
+            this.loginTool1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // loginTool1
+            // 
+            this.loginTool1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.loginTool1.Controls.Add(this.inventoryMultiTab1);
+            this.loginTool1.Location = new System.Drawing.Point(0, 0);
+            this.loginTool1.Name = "loginTool1";
+            this.loginTool1.Size = new System.Drawing.Size(1296, 163);
+            this.loginTool1.TabIndex = 0;
+            this.loginTool1.loginButtonClick += new System.EventHandler(this.LoginTool_loginButtonClick);
+            // 
+            // inventoryMultiTab1
+            // 
+            this.inventoryMultiTab1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.inventoryMultiTab1.Location = new System.Drawing.Point(0, 0);
+            this.inventoryMultiTab1.Name = "inventoryMultiTab1";
+            this.inventoryMultiTab1.Size = new System.Drawing.Size(1295, 682);
+            this.inventoryMultiTab1.TabIndex = 1;
+            this.inventoryMultiTab1.logoutButtonClick += new System.EventHandler(this.inventoryMultiTab1_LogoutClick);
+            this.inventoryMultiTab1.searchButtonClick += new System.EventHandler(this.inventoryMultiTab1_SearchClick);
+            this.inventoryMultiTab1.editButtonClick += new System.EventHandler(this.inventoryMultiTab1_EditClick);
+            this.inventoryMultiTab1.addItemButtonClick += new System.EventHandler(this.inventoryMultiTab1_AddItemClick);
+            this.inventoryMultiTab1.removeButtonClick += new System.EventHandler(this.inventoryMultiTab1_RemoveItemClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1295, 683);
+            this.Controls.Add(this.loginTool1);
             this.Name = "Form1";
             this.Text = "CKK Inventory Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.loginTool1.ResumeLayout(false);
+            this.loginTool1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private LoginTool loginTool1;
+
+        protected void LoginTool_loginButtonClick(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private InventoryMultiTab inventoryMultiTab1;
+
+        protected void inventoryMultiTab1_LogoutClick(object sender, System.EventArgs e)
+        {
+
+        }
+
+        protected void inventoryMultiTab1_SearchClick(object sender, System.EventArgs e)
+        {
+
+        }
+
+        protected void inventoryMultiTab1_EditClick(object sender, System.EventArgs e)
+        {
+
+        }
+
+        protected void inventoryMultiTab1_AddItemClick(object sender, System.EventArgs e)
+        {
+
+        }
+
+        protected void inventoryMultiTab1_RemoveItemClick(object sender, System.EventArgs e)
+        {
+
+        }
     }
 }
--- CKK.UI/Form1.cs
+++ CKK.UI/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CKK.Logic.Models;
 
 namespace CKK.GUI.WinForms
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            Store testStore = new Store();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
