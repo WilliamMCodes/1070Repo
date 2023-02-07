03bd9eb58b75504518913de68e58d64ef1e2671f
William Moulding <wtmoulding@comcast.net>
2023-02-06 17:42:18 -0700

    ProductRepository 1st Attempt

--- CKK.DB/Repository/ProductRepository.cs
+++ CKK.DB/Repository/ProductRepository.cs
@@ -1,41 +1,83 @@
 ﻿using CKK.DB.Interfaces;
 using CKK.Logic.Models;
+using Dapper;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Text;
 
 namespace CKK.DB.Repository
 {
     public class ProductRepository : IProductRepository
     {
-        public int Add(int id)
+        private readonly IConnectionFactory _connectionFactory;
+        public ProductRepository(IConnectionFactory conn)
         {
-            throw new NotImplementedException();
+            _connectionFactory = conn;
+        }
+        public int Add(Product entity)
+        {
+            var sql = "INSERT INTO Products (Price, Quantity, Name) VALUES (@Price,@Quantity, @Name)";
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                connection.Open();
+                var result = connection.Execute(sql, entity);
+                return result;
+            }
         }
 
         public int Delete(int id)
         {
-            throw new NotImplementedException();
+            var sql = "DELETE FROM Products WHERE Id = @Id";
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                connection.Open();
+                var result = connection.Execute(sql, new {Id = id});
+                return result;
+            }
         }
 
         public List<Product> GetAll()
         {
-            throw new NotImplementedException();
+            var sql = "SELECT * FROM Products";
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                connection.Open();
+                var list = connection.Query(sql);
+                return list;
+            }
         }
 
         public Product GetbyId(int id)
         {
-            throw new NotImplementedException();
+            var sql = "SELECT * FROM Products WHERE Id = @Id";
+            using(var connection = _connectionFactory.GetConnection)
+            {
+                connection.Open();
+                var product = connection.QuerySingleOrDefault(sql, new {Id = id});
+                return product;
+            }
         }
 
         public List<Product> GetByName(string name)
         {
-            throw new NotImplementedException();
+            var sql = "SELECT * FROM Products WHERE Name LIKE @Name";
+            using ( var connection = _connectionFactory.GetConnection)
+            {
+                connection.Open();
+                var result = connection.Query(sql, new {Name = "*" + name + "*" });
+                return  result;
+            }
         }
 
-        public int Update(int id)
+        public int Update(Product entity)
         {
-            throw new NotImplementedException();
+            var sql = "UPDATE Products SET Price = @Price, Quantity = @Quantity, Name = @Name WHERE Id = @Id";
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                connection.Open();
+                var result = connection.Execute(sql, entity);
+            }
         }
     }
 }
